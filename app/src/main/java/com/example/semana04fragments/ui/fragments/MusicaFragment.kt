b407b9098ffa5bb482c6ae511da577b47940f073
Luis Chang <luis.chang@outlook.com>
2023-09-30 10:50:05 -0500

    Add Adapter

--- app/src/main/java/com/example/semana04fragments/ui/fragments/MusicaFragment.kt
+++ app/src/main/java/com/example/semana04fragments/ui/fragments/MusicaFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.semana04fragments.R
+import com.example.semana04fragments.ui.model.SongModel
 
 
 class MusicaFragment : Fragment() {
@@ -14,6 +17,23 @@ class MusicaFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_musica, container, false)
+        val view: View =  inflater.inflate(R.layout.fragment_musica, container, false)
+
+        val rvMusica: RecyclerView = view.findViewById(R.id.rvMusica)
+
+        rvMusica.layoutManager = LinearLayoutManager(requireContext())
+
+
+        return view
     }
+
+    private fun ListSong(): List<SongModel>{
+        val lstSong: ArrayList<SongModel> = ArrayList()
+
+
+
+        return lstSong
+    }
+
+
 }
---
+++ app/src/main/java/com/example/semana04fragments/ui/model/SongModel.kt
@@ -0,0 +1,8 @@
+package com.example.semana04fragments.ui.model
+
+data class SongModel(val id: Int,
+                    val image: Int,
+                    val songName: String,
+                    val album: String,
+                    val numberPlay: String,
+                    val songTime: String)

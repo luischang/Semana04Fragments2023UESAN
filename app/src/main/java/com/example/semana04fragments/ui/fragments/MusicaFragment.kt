97e6da7ad6ce3b68ac4b92b4c20340652b2dcf19
Luis Chang <luis.chang@outlook.com>
2023-09-30 11:07:54 -0500

    Finish Adapter

--- app/src/main/java/com/example/semana04fragments/ui/fragments/MusicaFragment.kt
+++ app/src/main/java/com/example/semana04fragments/ui/fragments/MusicaFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.semana04fragments.R
+import com.example.semana04fragments.ui.adapter.SongAdapter
 import com.example.semana04fragments.ui.model.SongModel
 
 
@@ -22,15 +23,18 @@ class MusicaFragment : Fragment() {
         val rvMusica: RecyclerView = view.findViewById(R.id.rvMusica)
 
         rvMusica.layoutManager = LinearLayoutManager(requireContext())
-
-
+        rvMusica.adapter = SongAdapter(ListSong())
         return view
     }
 
     private fun ListSong(): List<SongModel>{
         val lstSong: ArrayList<SongModel> = ArrayList()
 
-
+        lstSong.add(SongModel(1,R.drawable.intheend,"In the end","Hybrid Theory","200,000","3:36"))
+        lstSong.add(SongModel(2,R.drawable.castleofglass,"Castle of Glass","Hybrid Theory","200,000","3:36"))
+        lstSong.add(SongModel(3,R.drawable.numb,"Numb","Hybrid Theory","200,000","3:36"))
+        lstSong.add(SongModel(4,R.drawable.onestepcloser,"One step closer","Hybrid Theory","200,000","3:36"))
+        lstSong.add(SongModel(5,R.drawable.whativedone,"What I've done","Hybrid Theory","200,000","3:36"))
 
         return lstSong
     }
